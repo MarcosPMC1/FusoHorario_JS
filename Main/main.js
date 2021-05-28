75c793541f6a0ed3fba908421fb89b29cffbc8f4
MarcosPMC1 <66838630+MarcosPMC1@users.noreply.github.com>
2021-05-28 11:22:45 -0300

    Correção de erros

--- Main/main.js
+++ Main/main.js
@@ -1,23 +1,31 @@
 function gtime(val){
     var time = new Date();
-    
-    var h = time.getHours();
-    var m = time.getMinutes();
-    var s = time.getSeconds();
-    
+    var h = time.getUTCHours();
+    var m = time.getUTCMinutes();
+    var s = time.getUTCSeconds();
+
+    var fh = val + h;
+
+    if (h <= val) {
+        fh = 24 - fh;
+    }
+    if (fh > 24) {
+        fh = fh - 24; 
+    }
     if (val === 0) {
         var id = "time0";
     }if (val !== 0) {
         var id  = "time" + val;
-    } 
+    }
     if (s<10) {
         s = "0" + s;
-    }else if (m<10) {
-        m = "0" + m;
-    }else if (h<10) {
-        h = "0" + Math.abs(h);
     }
-    console.log(h);
-    var cont = "<span>" + h + "h" + m + "m" + s + "s"// + "</span><h2>" + val + "</h2>";
+    if (m<10) {
+        m = "0" + m;
+    }
+    if (fh<10) {
+        fh = "0" + fh;
+    }
+    var cont = "<span>" + fh + "h" + m + "m" + s + "s" + "</span><h2>" + val + "</h2>";
     document.getElementById(id).innerHTML = cont;
 }
